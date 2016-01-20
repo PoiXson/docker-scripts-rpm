00a59f3c4f73fd1fb1adf5c58709dd90a5ad4779
lorenzop <lorenzo@poixson.com>
2016-01-20 04:17:24 -0500

    cleaned up and finished docker-build.sh script

--- src/docker-build.sh
+++ src/docker-build.sh
@@ -3,66 +3,46 @@ source /usr/bin/docker-scripts/docker-common.sh
 
 
 
-if [ ! -f /etc/docker-scripts.conf ]; then
-	errcho '/etc/docker-scripts.conf file not found!'
-	exit 1
-fi
-if [ ! -f "${PWD}/Dockerfile" ]; then
-	errcho 'Dockerfile not found in current directory!'
-	exit 1
-fi
-
-source /etc/docker-scripts.conf
-
-
-
-NAME=''
 IMAGE_NAME=''
-IMAGE_NAME_TAG=''
 IMAGE_VERSION=''
-function get_image_info() {
-	# get name comment from file
-	local line=$(head -n1 "${PWD}/Dockerfile")
-	if [[ $line == "# name: "* ]]; then
-		NAME=`echo "${line}" | rev | cut -d: -f1 | rev | xargs`
-	else
-		NAME=`echo "${PWD}" | rev | cut -d/ -f1 | rev | xargs`
-	fi
-	if [ -z $NAME ]; then
-		echo 'Failed to find a name for the image!'
+IMAGE_ORGNAME=''
+IMAGE_ORGNAMETAG=''
+function get_build_info() {
+	if [ ! -f "${PWD}/Dockerfile" ]; then
+		errcho 'Dockerfile not found in current directory!'
 		exit 1
 	fi
-	if [ -z $DOCKER_IMAGE_ORG ]; then
-		IMAGE_NAME="${NAME}"
-		IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_VERSION}"
-	else
-		IMAGE_NAME="${DOCKER_IMAGE_ORG}/${NAME}"
-		IMAGE_NAME_TAG="${IMAGE_NAME}:${IMAGE_VERSION}"
+	# get comments from Dockerfile
+	while read LINE; do
+		if [[ $LINE != "# "* ]]; then
+			break
+		fi
+		# name:
+		if [[ $LINE == "# name: "* ]]; then
+			IMAGE_NAME=`echo "${LINE}" | rev | cut -d: -f1 | rev | xargs`
+		elif [[ $LINE == "# version: "* ]]; then
+			IMAGE_VERSION=`echo "${LINE}" | rev | cut -d: -f1 | rev | xargs`
+		fi
+	done < "${PWD}/Dockerfile"
+	if [ -z $IMAGE_NAME ]; then
+		IMAGE_NAME=`echo "${PWD}" | rev | cut -d/ -f1 | rev | xargs`
 	fi
-	# get version comment from file
-	local line=$(head -n2 "${PWD}/Dockerfile" | tail -n1)
-	if [[ $line == "# version: "* ]]; then
-		IMAGE_VERSION=`echo "${line}" | rev | cut -d: -f1 | rev | xargs`
-	else
-		echo 'Version comment not found in Dockerfile!'
+	if [ -z $IMAGE_NAME ]; then
+		echo 'Failed to find a name for the image!'
 		exit 1
 	fi
 	if [ -z $IMAGE_VERSION ]; then
 		echo 'Failed to find image version from Dockerfile!'
 		exit 1
 	fi
+	IMAGE_ORGNAME="${DOCKER_IMAGE_ORG}/${IMAGE_NAME}"
+	IMAGE_ORGNAMETAG="${IMAGE_ORGNAME}:${IMAGE_VERSION}"
+	title "Creating Docker Image" "${IMAGE_ORGNAMETAG}"
+#	title "Creating Docker Image" "${DOCKER_IMAGE_ORG} / ${IMAGE_NAME} : ${IMAGE_VERSION}"
 }
-get_image_info
-echo
-echo 'Creating docker image..'
-if [ -z $DOCKER_IMAGE_ORG ]; then
-	echo "${NAME} : ${IMAGE_VERSION}"
-else
-	echo "${DOCKER_IMAGE_ORG} / ${NAME} : ${IMAGE_VERSION}"
-fi
-echo
+get_build_info
 
 
 
-docker build -t "${IMAGE_NAME_TAG}" "${PWD}/"
-docker tag -f "${IMAGE_NAME_TAG}" "${IMAGE_NAME}:latest"
+docker build -t "${IMAGE_ORGNAMETAG}" "${PWD}/"
+docker tag -f "${IMAGE_ORGNAMETAG}" "${IMAGE_NAME}:latest"
